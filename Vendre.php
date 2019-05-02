635a1d2799d4fbb96a1b8bfcad9b0d4c078393a7
Philippe-ece <36538799+Philippe-ece@users.noreply.github.com>
2019-05-02 23:05:30 +0200

    Update and rename Vendre.html to Vendre.php

--- Vendre.php
+++ Vendre.php
@@ -12,12 +12,12 @@
 					<li >
 						<img src="Images/menu.jpg">
 						<ul class="l2">
-							<li><a href="Categories.html" title="Page principale">Catégories</a></li>
-							<li><a href="VentesFlash.html" title="Accéder aux ventes flash">Ventes flash</a></li>
+							<li><a href="Categories.php" title="Page principale">Catégories</a></li>
+							<li><a href="VentesFlash.php" title="Accéder aux ventes flash">Ventes flash</a></li>
 							<li><a href="#" id="currentpage" title="Page actuelle">Vendre</a></li>
-							<li><a href="Compte.html" title="Accéder à votre compte">Votre compte</a></li>
-							<li><a href="Panier.html" title="Accéder au panier">Panier</a></li>
-							<li><a href="Admin.html" title="Espace réservé aux admins">Admin</a></li>
+							<li><a href="Compte.php" title="Accéder à votre compte">Votre compte</a></li>
+							<li><a href="Panier.php" title="Accéder au panier">Panier</a></li>
+							<li><a href="Admin.php" title="Espace réservé aux admins">Admin</a></li>
 						</ul>
 					</li>
 				</ul>
