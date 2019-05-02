e2ecdaece247360f21d6230fcd8fa87c529654c5
TerencePL <36531921+TerencePL@users.noreply.github.com>
2019-05-02 16:21:01 +0200

    ECEbay-Vente
    Passage en PHP + modif des liens en consequence

--- Vente/Musique.php
+++ Vente/Musique.php
@@ -1,84 +1,61 @@
 <?php
-
-//mettre les données recupérées  dans la base de données
-    
-   $dbname = "ecebay";
-   $db_login = "root";
-   $db_pass	= "";
-
-$database= "ecebay";
-//Rappel:votre serveur = localhost | votre login = root | votre mot de pass = ''(rien)
-$db_handle = mysqli_connect('localhost', 'root', '');
-$db_found = mysqli_select_db($db_handle, $database);
-
-
-//idmax recupère l'ID_Produit max deja existant.
-$result = mysqli_query($db_handle, "SELECT * FROM `produit` ORDER BY `ID_Produit` DESC LIMIT 1");
-$row = mysqli_fetch_array($result);
-$idmax=$row['ID_Produit'];
-
-
-
-
-$ID_Produit=$idmax+1;
-$ID_Vendeur=0;
-$Nom = isset($_POST['nom']) ? $_POST['nom']:"";
-$Prix = isset($_POST["prix"]) ? $_POST["prix"]:"";
-$Stock = isset($_POST["stock"]) ? $_POST["stock"]:"";
-
-$Description = isset($_POST["description"]) ? $_POST["description"]:"";
-$Auteur = isset($_POST["auteur"]) ? $_POST["auteur"]:"";
-$Date_Parution = isset($_POST["dateparution"]) ? $_POST["dateparution"]:"";
-$Genre = isset($_POST["genre"]) ? $_POST["genre"]:"";
-
-
-//$Nom="test";
-
-
-$Verif=0;
-
-/*Verifier si le mail du nouveau profil existe déjà ou non dans la bdd
-if($db_found) {
-
-	$sql = "SELECT * FROM vendeur";
-	$result = mysqli_query($db_handle,$sql);
-	while($data = mysqli_fetch_assoc($result)) 
-		{
-		if($Mail == $data['Mail'])
-			{$Verif=1;}
-		}
-	} */
-
-//inserer dans la table produit
-if($Verif==0)
-{
-		$bdd = new PDO("mysql:host=localhost;dbname=$dbname;charset=UTF8", $db_login, $db_pass);
-		$sql = "INSERT INTO produit (ID_Vendeur, ID_Produit, Nom, Prix, Description, Stock) VALUES(";
-		$sql .= "'$ID_Vendeur', '$ID_Produit', '$Nom', '$Prix', '$Description', '$Stock' )";
-	
-		$bdd->query($sql);
-}
-else if ($Verif==1)
-{echo "Mail déjà existant. <br> <br>";}
-
-
-//Inserer dans la table musique
-if($Verif==0)
-{
-		$bdd = new PDO("mysql:host=localhost;dbname=$dbname;charset=UTF8", $db_login, $db_pass);
-		$sql = "INSERT INTO musique (ID_Produit, Auteur, Date_Parution, Genre) VALUES(";
-		$sql .= "'$ID_Produit', '$Auteur', '$Date_Parution', '$Genre')";
-	
-		$bdd->query($sql);
-}
-else if ($Verif==1)
-{echo "Mail déjà existant. <br> <br>";}
-
-
-
-
-
-
-
-mysqli_close($db_handle);
+session_start(); // On démarre la session AVANT toute chose
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vente Musique</title>
+	
+</head>
+
+	<body>
+
+		<form action="http://localhost/ECEbay/Vente/MusiqueAjout.php" method="post">
+			<table>
+				<tr>
+					<td>Nom:</td>
+					<td><input type="text" name="nom"></td>
+				</tr>
+				<tr>
+					<td>Prix:</td>
+					<td><input type="float" name="prix"></td>
+				</tr>	
+				<tr>
+					<td>Stock:</td>
+					<td><input type="int" name="stock"></td>
+				</tr>
+				<tr>
+				<tr>
+					<td>Auteur:</td>
+					<td><input type="text" name="auteur"></td>
+				</tr>
+				<tr>
+					<td>Date de parution:</td>
+					<td><input type="date" name="dateparution"></td>
+				</tr>	
+				<tr>
+					<td>Description:</td>
+					<td><textarea name="description"></textarea> </td>
+				</tr>
+
+				<tr>
+				       <td><label for="genre">Quelle genre de musique vendez vous ?</label><br/>
+       						<select name="genre">
+           						<option value="rap">Rap</option>
+           						<option value="edm">EDM</option>
+           						<option value="dnb">Drum & Bass</option>
+           						<option value="techno">Techno</option>
+       						</select></td>
+       			</tr>
+
+				<tr>
+					<td colspan="2" align="center"><input type="submit" n="Valider"></td>
+				</tr>
+			</table>
+		</form>
+		</p>
+
+	</body>
+</html>
+
